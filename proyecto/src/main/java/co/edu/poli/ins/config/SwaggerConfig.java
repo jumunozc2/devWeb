55b4ea2d40ef8accfaf39a993003c3032a3898b2
jecastillo13 <47476526+jecastillo13@users.noreply.github.com>
2021-03-29 15:41:09 -0500

    Swagger
    Implemnetacion de los swagger

---
+++ proyecto/src/main/java/co/edu/poli/ins/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package co.edu.poli.ins.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket customerApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+                .select()                 
+                .apis(RequestHandlerSelectors.basePackage("co.edu.poli.ins.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(MySQLCustomerApiInfo());
+	}
+	
+	private ApiInfo MySQLCustomerApiInfo() {
+		return new ApiInfoBuilder().
+				title("MySQL REST API").
+				description("Customer REST API")
+				.contact(new Contact("Web App MySQL", "", "jecastillo13@poligran.edu.co , jumunoz@poligran.edu.co")).
+				version("0.0.1").
+				build();
+	}
+	
+	
+}
